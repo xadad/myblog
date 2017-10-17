db1e5c3112a513b71f3646f212b6bd8c26d6188f
Смольников Максим Федорович <max@rcoko27.ru>
2017-10-17 15:18:32 +1000

    add first test

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,9 @@ class Entry(models.Model):
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     modified_at = models.DateTimeField(auto_now=True, editable=False)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = "entries"
--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Entry
+
+
+class EntryModelTest(TestCase):
+
+    def test_string_representation(self):
+        entry = Entry(title="My entry title")
+        self.assertEqual(str(entry), entry.title)
+    
+    def test_verbose_name_plural(self):
+    self.assertEqual(str(Entry._meta.verbose_name_plural), "entries")
