eb4e8ee3a730f1bc0674089420069958217f381c
Смольников Максим Федорович <max@rcoko27.ru>
2017-10-17 13:56:14 +1000

    add model blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Entry(models.Model):
+    title = models.CharField(max_length=500)
+    author = models.ForeignKey('auth.User')
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    modified_at = models.DateTimeField(auto_now=True, editable=False)
